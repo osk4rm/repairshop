e149fef62f8de5cfd359a96ba39169e6c89c1db8
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2020-07-11 00:32:20 +0200

    added separate views for admin and employee, role authentication, pre-login view

--- src/main/java/com/wsiiz/repairshop/application/ui/MainUI.java
+++ src/main/java/com/wsiiz/repairshop/application/ui/MainUI.java
@@ -1,15 +1,16 @@
 package com.wsiiz.repairshop.application.ui;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.View;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.spring.navigator.SpringNavigator;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Component;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
+import com.wsiiz.repairshop.foundation.ui.BaseView;
 import com.wsiiz.repairshop.foundation.ui.i18n.I18nAware;
+import com.wsiiz.repairshop.servicing.domain.service.Service;
 import com.wsiiz.repairshop.servicing.ui.service.ServiceView;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.vaadin.viritin.layouts.MHorizontalLayout;
 import org.vaadin.viritin.layouts.MVerticalLayout;
 
@@ -17,40 +18,28 @@ import org.vaadin.viritin.layouts.MVerticalLayout;
 @Theme("repairshop")
 public class MainUI extends UI implements I18nAware {
 
-  MainViewDisplay mainContent;
+    MainViewDisplay mainContent;
 
-  public MainUI(MainViewDisplay mainContent, SpringNavigator navigator) {
-    this.mainContent = mainContent;
-    navigator.setErrorView(ServiceView.class);
-  }
+    public MainUI(MainViewDisplay mainContent, SpringNavigator navigator) {
+        this.mainContent = mainContent;
+        navigator.setErrorView(ServiceView.class);
+    }
 
-  @Override
-  protected void init(VaadinRequest request) {
+    @Override
+    protected void init(VaadinRequest request) {
 
-    setContent(
-        new MHorizontalLayout()
-            .add(createNavigationBar())
+        Button buttonAdmin = new Button("ADMIN PANEL");
+        Button buttonEmployee = new Button("EMPLOYEE PANEL");
 
-            .expand(mainContent)
-            .withFullHeight()
-    );
-  }
+        buttonAdmin.addClickListener(event -> getUI().getPage().setLocation("admin-panel"));
+        buttonEmployee.addClickListener(event -> getUI().getPage().setLocation("employee-panel"));
 
-  private Component createNavigationBar() {
-    MVerticalLayout m = new MVerticalLayout().withWidth("150px").withStyleName("main-menu");
-    m.addComponent(createNavButton(i18n("service"), "service"));
-    m.addComponent(createNavButton(i18n("customer"), "customer"));
-    m.addComponent(createNavButton(i18n("employee"), "employee"));
-    return m;
-  }
+        setContent(
+                new MHorizontalLayout()
+                        .add(buttonAdmin, buttonEmployee)
 
-  private Component createNavButton(String caption, String path) {
-    Button button = new Button(caption);
-    button.addClickListener(e -> getNavigator().navigateTo(path));
-    button.addStyleName("main-menu-option");
-    button.addStyleName("main-menu-option-" + path);
-    return button;
-  }
+        );
+    }
 
 
 }
--- src/main/java/com/wsiiz/repairshop/security/config/WebSecurityConfig.java
+++ src/main/java/com/wsiiz/repairshop/security/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,10 +18,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    UserAccountRepository userAccountRepository;
 
     @Autowired
     UserDetailsServiceImpl userDetailsService;
@@ -33,18 +32,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").authenticated()
+                .antMatchers("/").permitAll()
+                .antMatchers("/employee-panel").hasAuthority("EMPLOYEE")
+                .antMatchers("/admin-panel").hasAuthority("ADMINISTRATOR")
                 .and()
-                .formLogin().permitAll();
+                .formLogin().permitAll()
+                .and()
+                .logout().logoutSuccessUrl("/")
+                .and()
+                .csrf().disable();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-//    @EventListener(ApplicationReadyEvent.class)
-//    public void add(){
-//        UserAccount user = new UserAccount("user1", passwordEncoder().encode("user1"), UserKind.EMPLOYEE);
-//        userAccountRepository.save(user);
-//    }
+
 }
